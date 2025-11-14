07eb7323b5d21ca98a57fd9b92ced2d0cd7ef849
Steven Wijaya <stewi2kstudy@gmail.com>
2025-11-14 16:12:00 +0700

    feat : global helper response and middleware log

--- router/router.go
+++ router/router.go
@@ -2,10 +2,13 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/stevenwijaya/finance-tracker/middleware"
 )
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(middleware.LoggerMiddleware())
 
 	//tambahkan router modular disini
 	InitUserRouter(router)
